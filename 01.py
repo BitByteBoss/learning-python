4f4a95982c6ac68c6a3ef1bbd25023cae9d0712b
Dheeraj Dixit <160017238+BitByteBoss@users.noreply.github.com>
2024-08-24 23:57:26 +0530

    basics complete

---
+++ 01.py
@@ -0,0 +1,27 @@
+# Exercise: Syntax, Variables, and Numbers
+
+
+# print("You've successfully run some Python code")
+# print("Congratulations!")
+
+
+# from learntools.core import binder; binder.bind(globals())
+# from learntools.python.ex1 import *
+# print("Setup complete! You're ready to start question 0.")
+
+# color = 'blue'
+# q0.check()
+
+
+
+# pi = 3.14159 # approximate
+# diameter = 3
+
+# # Create a variable called 'radius' equal to half the diameter
+# radius = diameter/2
+
+# # Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+# area = pi * (radius**2)
+
+# # Check your answer
+# q1.check()
