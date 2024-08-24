05d0e2e7ff1f5af2d054de07e5c9c4da3811d5ec
Dheeraj Dixit <160017238+BitByteBoss@users.noreply.github.com>
2024-08-25 00:59:42 +0530

    02.py file complete

--- 02.py
+++ 02.py
@@ -1,11 +1,38 @@
-def least_difference(a, b, c):
-    diff1 = abs(a - b)
-    diff2 = abs(b - c)
-    diff3 = abs(a - c)
-    return min(diff1, diff2, diff3)
+# SETUP. You don't need to worry for now about what this code does or how it works.
+# from learntools.core import binder; binder.bind(globals())
+# from learntools.python.ex2 import *
+# print('Setup complete.')
 
-print(
-    least_difference(1, 10, 100),
-    least_difference(1, 10, 10),
-    least_difference(5, 6, 7), # Python allows trailing commas in argument lists. How nice is that?
-)
+# def round_to_two_places(num):
+# #     help(round)
+#     return round(num, 2)
+    # """Return the given number rounded to two decimal places. 
+    
+    # >>> round_to_two_places(3.14159)
+    # 3.14
+#     """
+#     # Replace this body with your own code.
+#     # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+#     # because after we begin a code block, Python requires at least one line of code)
+# #     pass
+# # round_to_two_places(3.14159)
+# # Check your answer
+# # q1.check()
+
+# Put your test code here
+# round(314345, -2)
+
+
+# def to_smash(total_candies, n=3):
+#     """Return the number of leftover candies that must be smashed after distributing
+#     the given number of candies evenly between 3 friends.
+    
+#     >>> to_smash(91)
+#     1
+#     """
+#     return total_candies % n
+
+
+
+# # Check your answer
+# q3.check()
---
+++ 03.py
@@ -0,0 +1,27 @@
+# # Your code goes here. Define a function called 'sign'
+# def sign(x):
+#     if x > 0:
+#         return 1
+#     elif x < 0:
+#         return -1
+#     else:
+#         return 0
+# # Check your answer
+# q1.check()
+
+# def to_smash(total_candies):
+#     """Return the number of leftover candies that must be smashed after distributing
+#     the given number of candies evenly between 3 friends.
+    
+#     >>> to_smash(91)
+#     1
+#     """
+#     if total_candies == 1:
+#         print("splitting 1 candy")
+#     else:
+#         print("Splitting", total_candies, "candies")
+    
+#     return total_candies % 3
+
+# to_smash(91)
+# to_smash(1)
